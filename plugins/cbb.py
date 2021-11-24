e6ddcce8ab71ede0391e55a7ebf1551f8d97ee92
Riebot <86209111+Riebot@users.noreply.github.com>
2021-11-25 00:50:46 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Owner : <a href='tg://user?id={OWNER_ID}'>Sanz</a>\n○ Language : <code>Python3</code>\n○ Support Channel : @asupanfree69\n○ Support group : @free69rebon</b>",
+            text = f"<b>👑 Owner : <a href='tg://user?id={OWNER_ID}'>Sanz</a>\n💦 Donasi melaui bot : @Onlyfun18donasi_bot\n💥 Support group : @onlyfun_18</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
