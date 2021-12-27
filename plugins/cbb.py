b118f67add5ec80659a65d1567ad9d713edabbed
Riebot <86209111+Riebot@users.noreply.github.com>
2021-12-27 16:54:14 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,6 +1,8 @@
 #(©)Codexbotz
 
-from pyrogram import __version__
+import asyncio
+import base64
+
 from bot import Bot
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
