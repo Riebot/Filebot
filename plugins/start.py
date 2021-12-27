f4a05e4e7a16543f3401828cd149436ef7615541
Riebot <86209111+Riebot@users.noreply.github.com>
2021-12-27 08:26:58 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -95,11 +95,11 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    text = "<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>"
+        text = "<b>Kamu harus join atau gabung ke channel terlebih dahulu agar bisa menonton videonya! Klik Disini jika sudah join dan '>Coba Lagi</a>\n\njoin Channel Atau Group</b>"
     message_text = message.text
     try:
         command, argument = message_text.split()
-        text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>try again</a></b>"
+        text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>Coba Lagi</a></b>"
     except ValueError:
         pass
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url = client.invitelink)]])
