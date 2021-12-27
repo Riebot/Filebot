3cea5fd359c23e9498ed7d6f1f072dd9f6f0808b
Riebot <86209111+Riebot@users.noreply.github.com>
2021-12-27 07:49:33 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>👑 Owner bot : <a href='tg://user?id={OWNER_ID}'>ʜᴧʀ• [ʀᴇʜᴀᴛ]</a>\n💦 Channel : @kiwpiwsex\n☕ Manage by : @SilenceSpe4ks</b>",
+            text = f"<b>👑 Owner bot : <a href='tg://user?id={OWNER_ID}'>ʜᴧʀ• [ʀᴇʜᴀᴛ]</a>\n💦 Channel : @kiwpiwsex\n☕ Manage by : <a href='https://t.me/SilenceSpe4ks'>Klik Disini</a></b>\n",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
