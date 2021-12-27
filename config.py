ee5691e69a00188af792d8e0ac27115a410ff8b4
Riebot <86209111+Riebot@users.noreply.github.com>
2021-12-27 08:18:09 +0700

    Update config.py

--- config.py
+++ config.py
@@ -22,6 +22,9 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
+# Database
+DB_URI = os.environ.get("DATABASE_URL", "")
+
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
