6feb7da03ed7ccfadbe9f3d4eb5e950a4dcb7ce8
Riebot <86209111+Riebot@users.noreply.github.com>
2021-12-27 10:32:14 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -72,13 +72,11 @@ async def start_command(client: Client, message: Message):
         return
     else:
         reply_markup = InlineKeyboardMarkup(
-            [
                 [
                     InlineKeyboardButton("📑 ᴛᴇɴᴛᴀɴɢ sᴀʏᴀ", callback_data = "about"),
-                    InlineKeyboardButton("ᴛᴜᴛᴜᴘ ❌", callback_data = "close")
-                ]
+                    InlineKeyboardButton("ᴛᴜᴛᴜᴘ ❌", callback_data = "close"),
+                ],
             ]
-        )
         await message.reply_text(
             text=START_MSG.format(
                 first=message.from_user.first_name,
